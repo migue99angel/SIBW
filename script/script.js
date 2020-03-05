520a23e066a78759a0cf52a3fc644bfff26b8277
Miguel Angel <posadasarraez@gmail.com>
2020-03-05 16:35:02 +0100

    Empezando P2

---
+++ script/script.js
@@ -0,0 +1,6 @@
+function mostrarCajonComentario()
+{
+    document.getElementsByClassName("CajonComen")[0].style.visibility = "visible"
+    document.getElementsByClassName("CajonComen")[0].style.margin = 0;
+
+}
