6da19fe2043b8de6f668f428289e8f619f759b60
Miguel Angel <posadasarraez@gmail.com>
2020-03-05 17:31:38 +0100

    Añadiendo documentación

--- script/script.js
+++ script/script.js
@@ -1,6 +1,8 @@
+/*Función que muestra el cajón de comentarios*/
 function mostrarCajonComentario()
 {
+    /*Cambio la propiedad visibility a visible para que el formulario se muestre*/
     document.getElementsByClassName("CajonComen")[0].style.visibility = "visible"
+    /*Pongo el margin a que se vea más bonito*/
     document.getElementsByClassName("CajonComen")[0].style.margin = 0;
-
 }
